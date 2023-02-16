6d3da08c02842c52a43e4c3aef829be2db9237f4
Giovanbattista Altobelli <124137861+Giamba001@users.noreply.github.com>
2023-02-16 21:17:55 +0100

    ClassicModels Exercises
    last

---
+++ mysqlOnline/script.js
@@ -0,0 +1,91 @@
+function returnHome() {
+    location.href="index.html";
+}
+
+function esercizioUno() {
+    location.href = "esercizioUno.html";
+}
+
+function esercizioDue() {
+    location.href = "esercizioDue.html";
+}
+
+function esercizioTre() {
+    location.href = "esercizioTre.html";
+}
+
+function esercizioQuattro() {
+    location.href = "esercizioQuattro.html";
+}
+
+function esercizioCinque() {
+    location.href = "esercizioCinque.html";
+}
+
+function esercizioSei() {
+    location.href = "esercizioSei.html";
+}
+
+function esercizioSette() {
+    location.href = "esercizioSette.html";
+}
+
+function esercizioOtto() {
+    location.href = "esercizioOtto.html";
+}
+
+function esercizioNove() {
+    location.href = "esercizioNove.html";
+}
+
+function esercizioDieci() {
+    location.href = "esercizioDieci.html";
+}
+
+function esercizioUndici() {
+    location.href = "esercizioUndici.html";
+}
+
+function esercizioDodici() {
+    location.href = "esercizioDodici.html";
+}
+
+function esercizioTredici() {
+    location.href = "esercizioTredici.html";
+}
+
+function esercizioQuattordici() {
+    location.href = "esercizioQuattordici.html";
+}
+
+function esercizioQuindici() {
+    location.href = "esercizioQuindici.html";
+}
+
+function esercizioSedici() {
+    location.href = "esercizioSedici.html";
+}
+
+function esercizioDiciasette () {
+    location.href = "esercizioDiciasette.html"
+}
+
+function esercizioDiciotto() {
+    location.href = "esercizioDiciotto.html"
+}
+
+function esercizioDiciannove() {
+    location.href = "esercizioDiciannove.html"
+}
+
+function esercizioVenti() {
+    location.href = "esercizioVenti.html"
+}
+
+function esercizioVentuno() {
+    location.href = "esercizioVentuno.html"
+}
+
+function esercizioVentidue() {
+    location.href = "esercizioVentidue.html"
+}
